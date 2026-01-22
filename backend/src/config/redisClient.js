886ade5ef5ae65577118f0539773e86f1c6d65c6
mithun24112005 <mithun24112005@gmail.com>
2026-01-22 10:29:44 +0530

    redis working now

--- backend/src/config/redisClient.js
+++ backend/src/config/redisClient.js
@@ -1,12 +1,12 @@
 import { createClient } from "redis";
 
 const client = createClient({
-  username: "default",
-  password: process.env.REDIS_PASSWORD, 
-  socket: {
-    host: "redis-14594.crce217.ap-south-1-1.ec2.cloud.redislabs.com",
-    port: 14594,
-  },
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: 'redis-16365.crce182.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 16365
+    }
 });
 
 // Event listeners for connection status
